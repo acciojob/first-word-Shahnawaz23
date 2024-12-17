cd6fdc666680c7061948e0fb87d2c27ec824e124
Shahnawaz Akhtar <58129001+Shahnawaz23@users.noreply.github.com>
2024-12-18 00:00:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstWord(s) {
   // your code here
+	str = str.trim();
+
+    if (str === '') {
+        return '';
+    }
+
+    const spaceIndex = str.indexOf(' ');
+
+    if (spaceIndex === -1) {
+        return str;
+    }
+
+    // Return the substring from the start to the first space
+    return str.substring(0, spaceIndex);
 }
 
 // Do not change the code below
